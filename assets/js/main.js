26614a4bce41395220a43f6eb3bc19ee42edcc01
ali <applealijafaripour@gmail.com>
2025-03-21 11:41:56 +0330

    timer js script and design first part of nav-tabs

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+var countDownDate = new Date("March 23, 2025 18:00:00").getTime();
+
+var x = setInterval(function () {
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML =
+    '<span id="day">' +
+    days +
+    "</span>" +
+    '<span id="hours">' +
+    hours +
+    "</span>" +
+    '<span id="minutes">' +
+    minutes +
+    "</span>" +
+    '<span id="seconds">' +
+    seconds +
+    "</span>";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "تخفیف به پایان رسیده است";
+  }
+}, 1000);
