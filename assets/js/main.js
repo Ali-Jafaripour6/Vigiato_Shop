1e9f4cb305bf32481e2c298697325d422033092e
ali <applealijafaripour@gmail.com>
2025-03-22 12:03:05 +0330

    special offer design complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("April 30, 2025 18:00:00").getTime();
+var countDownDate = new Date("April 2, 2025 18:00:00").getTime();
 
 var x = setInterval(function () {
   var now = new Date().getTime();
@@ -10,28 +10,64 @@ var x = setInterval(function () {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("demo").innerHTML =
-    '<span id="day">' +
-    days +
-    "</span>" +
-    '<span id="hours">' +
-    hours +
-    "</span>" +
-    '<span id="minutes">' +
-    minutes +
-    "</span>" +
-    '<span id="seconds">' +
-    seconds +
-    "</span>";
+  // document.getElementById("demo").innerHTML =
+  //   '<span id="day">' +
+  //   days +
+  //   "</span>" +
+  //   '<span id="hours">' +
+  //   hours +
+  //   "</span>" +
+  //   '<span id="minutes">' +
+  //   minutes +
+  //   "</span>" +
+  //   '<span id="seconds">' +
+  //   seconds +
+  //   "</span>";
+
+  var z = document.getElementsByClassName("demos");
+  for (var i = 0; i < z.length; i++) {
+    z[i].innerHTML =
+      '<span id="day">' +
+      days +
+      "</span>" +
+      '<span id="hours">' +
+      hours +
+      "</span>" +
+      '<span id="minutes">' +
+      minutes +
+      "</span>" +
+      '<span id="seconds">' +
+      seconds +
+      "</span>";
+  }
 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "";
-    document
-      .getElementById("after-expire")
-      .setAttribute("id", "offer-expire-text");
-    document.getElementById("offerblur").style.filter = "blur(5px)";
-    document.getElementById("offer-expire-text").innerHTML =
-      "تخفیف به پایان رسیده است";
+    // document.getElementById("demo").innerHTML = "";
+    var y = document.getElementsByClassName("demos");
+    for (var i = 0; i < z.length; i++) {
+      y[i].innerHTML = "";
+    }
+
+    // document
+    //   .getElementById("after-expire")
+    //   .setAttribute("id", "offer-expire-text");
+    var g = document.getElementsByClassName("after-expire");
+    for (var i = 0; i < z.length; i++) {
+      g[i].classList.add("offer-expire-text");
+    }
+
+    // document.getElementById("offer-expire-text").innerHTML =
+    //   "تخفیف به پایان رسیده است";
+    var d = document.getElementsByClassName("offer-expire-text-inner");
+    for (var i = 0; i < z.length; i++) {
+      d[i].innerHTML = "تخفیف به پایان رسیده است";
+    }
+
+    // document.getElementById("offerblur").style.filter = "blur(5px)";
+    var t = document.getElementsByClassName("offerblur");
+    for (var i = 0; i < z.length; i++) {
+      t[i].style.filter = "blur(5px)";
+    }
   }
 }, 1000);
